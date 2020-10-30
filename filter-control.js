142de78ad35f737c40bf2c1f78b5af7ae25df7b1
Sean McCullough <banksean@gmail.com>
2020-10-29 20:52:00 -0700

    ¯\_(ツ)_/¯

---
+++ filter-control.js
@@ -0,0 +1,50 @@
+import {
+    LitElement,
+    html,
+    css
+} from "https://unpkg.com/lit-element/lit-element.js?module";
+
+import './param-slider';
+
+class FilterControl extends LitElement {
+    constructor() {
+        super();
+        this.cut = 1000;
+        this.q = 0.5;
+    }
+
+    static get properties() {
+        return {
+            cut: { type: Number }, //, hasChanged: this._valueChanged },
+            q: { type: Number }, //, hasChanged: this._valueChanged }
+        };
+    }
+
+    _cutChanged(evt) {
+        console.log('cut changed', evt.target, evt.target.value);
+        this.cut = evt.target.value;
+    }
+
+    _qChanged(evt) {
+        console.log('q changed', evt.target, evt.target.value);
+        this.q = evt.target.value;
+    }
+
+    static get style() {
+        return css `
+        `;
+    }
+
+    render() {
+        return html `
+        <div class="vcf">
+        VCF
+            <param-slider @input=${this._cutChanged} value=${this.cut} name="Cut" min="1" max="20000"></param-slider>
+            <param-slider @input=${this._qChanged} value=${this.q} name="Q" min="0.0" max="1.0" step="any"></param-slider>
+        </div>
+        `;
+    }
+}
+
+
+customElements.define('filter-control', FilterControl);
